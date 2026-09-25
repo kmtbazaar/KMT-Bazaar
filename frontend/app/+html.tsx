4b87033b76da7cdc30c47593c93dde861bd72b5d
kmtbazaar <km65746@gmail.com>
2026-09-25 21:33:47 +0530

    Prevent stale web build caching

--- frontend/app/+html.tsx
+++ frontend/app/+html.tsx
@@ -8,6 +8,9 @@ export default function Root({ children }: PropsWithChildren) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
